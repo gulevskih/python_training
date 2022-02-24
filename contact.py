25ee09b74c6aee5d9409671740425c2844d0b14f
Stanislav <gulevskih@mail.ru>
2022-02-24 21:23:13 +0300

    изменение названия некоторых переменных на более информативные; параметр Contact/Group изменён на contact/group, чтобы не было конфликта с именем класса

--- contact.py
+++ contact.py
@@ -1,7 +1,7 @@
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile,
-                 work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, phone_home, mobile,
+                 phone_work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
                  address2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
@@ -10,9 +10,9 @@ class Contact:
         self.title = title
         self.company = company
         self.address = address
-        self.home = home
+        self.phone_home = phone_home
         self.mobile = mobile
-        self.work = work
+        self.phone_work = phone_work
         self.fax = fax
         self.email = email
         self.email2 = email2
--- test_add_group.py
+++ test_add_group.py
@@ -34,19 +34,19 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, Group):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(Group.name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(Group.header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group page
         wd.find_element_by_name("submit").click()
 
