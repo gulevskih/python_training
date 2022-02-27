2d4b48fff02cf8a825638ca8dfef925947486357
Stanislav <gulevskih@mail.ru>
2022-02-27 14:24:03 +0300

    выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,12 +10,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username='admin', password='secret')
+    app.session.login(username='admin', password='secret')
     app.create_group(Group(name="qwer", header="asdf", footer="zxcv"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username='admin', password='secret')
+    app.session.login(username='admin', password='secret')
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
