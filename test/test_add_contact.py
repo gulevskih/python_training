e066062ac39182d3cc918b4049fd777e9504082c
Stanislav <gulevskih@mail.ru>
2022-02-27 16:28:39 +0300

    выделен класс для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,12 +10,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login('admin', 'secret')
-    app.create_new_contact(Contact(firstname="qwerty", middlename="zxcvb", lastname="asdfgh",
-                                       nickname='ritkgkb', title='vndkti', company='auriga', address='lj346',
-                                       phone_home='831954824', mobile='945934562', phone_work='911', fax='ax',
-                                       email="@adsf", email2="@qwer", email3="@zxcv", homepage='wwwjklcom',
-                                       bday='13', bmonth='September', byear="1984",
-                                       aday='4', amonth='May', ayear="1977",
-                                       address2='viekdl323', phone2='905234534', notes='theend'))
-    app.logout()
+    app.session.login('admin', 'secret')
+    app.contact.create(Contact(firstname="qwerty", middlename="zxcvb", lastname="asdfgh",
+                               nickname='ritkgkb', title='vndkti', company='auriga', address='lj346',
+                               phone_home='831954824', mobile='945934562', phone_work='911', fax='ax',
+                               email="@adsf", email2="@qwer", email3="@zxcv", homepage='wwwjklcom',
+                               bday='13', bmonth='September', byear="1984",
+                               aday='4', amonth='May', ayear="1977",
+                               address2='viekdl323', phone2='905234534', notes='theend'))
+    app.session.logout()
