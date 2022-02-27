6475c75ceb9cd468cbf5a2c77b8f0a1813c96477
Stanislav <gulevskih@mail.ru>
2022-02-27 16:17:30 +0300

    выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username='admin', password='secret')
-    app.create_group(Group(name="qwer", header="asdf", footer="zxcv"))
+    app.group.create(Group(name="qwer", header="asdf", footer="zxcv"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username='admin', password='secret')
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
