b1102afbe262bb447e3868fa25929a03f647ae21
Stanislav <gulevskih@mail.ru>
2022-03-01 20:04:00 +0300

    добавление теста на изменение первого в списке контакта

---
+++ test/test_edit_contact.py
@@ -0,0 +1,14 @@
+
+from model.contact import Contact
+
+
+def test_edit_contact(app):
+    app.session.login('admin', 'secret')
+    app.contact.edit_first_contact(Contact(firstname="", middlename="", lastname="",
+                               nickname='', title='', company='', address='',
+                               phone_home='', mobile='', phone_work='', fax='',
+                               email="", email2="", email3="", homepage='',
+                               bday='13', bmonth='September', byear="1984",
+                               aday='4', amonth='May', ayear="1977",
+                               address2='', phone2='', notes=''))
+    app.session.logout()
