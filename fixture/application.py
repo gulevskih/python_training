2fdcad5707cfe9e76b8e7ff08be55c9b27193cdd
Stanislav <gulevskih@mail.ru>
2022-03-10 20:09:41 +0300

    повышение "интеллектуальности" функций login и logout

--- fixture/application.py
+++ fixture/application.py
@@ -7,10 +7,14 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
 
     def is_valid(self):
         try:
@@ -18,10 +22,6 @@ class Application:
             return True
         except:
             return False
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
