cfb5a13b5a470083047761444a0e56b7825de08f
Stanislav <gulevskih@mail.ru>
2022-03-19 19:54:51 +0300

    проверка за счёт подсчёта групп до и после действий

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,7 +4,10 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
