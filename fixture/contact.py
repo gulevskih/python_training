75b498983b130189f1784516bcc1ad5fea718f2c
Stanislav <gulevskih@mail.ru>
2022-03-23 20:23:39 +0300

    update проверки тестов за счёт сравнения содержания списков (+firstname, +lastname)

--- fixture/contact.py
+++ fixture/contact.py
@@ -108,8 +108,10 @@ class ContactHelper:
             wd = self.app.wd
             self.return_home_page()
             self.contact_cashe = []
-            for element in wd.find_elements_by_name("entry"):
-                #text = element.find_element_by_name("selected[]").get_attribute("title")
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cashe.append(Contact(id=id))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                self.contact_cashe.append(Contact(firstname=firstname, lastname=lastname, id=id))
         return list(self.contact_cashe)
--- model/contact.py
+++ model/contact.py
@@ -35,10 +35,11 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
-        return self.id is None or other.id is None or self.id == other.id
+        return (self.id is None or other.id is None or self.id == other.id)\
+               and (self.firstname == other.firstname and self.lastname == other.lastname)
 
     def id_or_max(self):
         if self.id:
