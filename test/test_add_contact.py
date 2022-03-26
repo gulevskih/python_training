d0fbb236a58a2ed79d2eabe2a25eb1c2ef58f5f8
Stanislav <gulevskih@mail.ru>
2022-03-26 16:28:10 +0300

    тест для проверки телефонов контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="6", middlename="zxcvb", lastname="asdfgh",
+    contact = Contact(firstname="Sten", middlename="Platonovich", lastname="Li",
                       nickname='ritkgkb', title='vndkti', company='auriga', address='lj346',
                       homephone='(831)954824', mobilephone='945 934 562', workphone='(911) 000-28', fax='ax',
                       email="@adsf", email2="@qwer", email3="@zxcv", homepage='wwwjklcom',
