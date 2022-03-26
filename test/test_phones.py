48ae4c4cb627b3e6929818b4d01d51daf5381720
Stanislav <gulevskih@mail.ru>
2022-03-26 22:05:11 +0300

    update: проверка телефонов для случайного контакта

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,13 +1,18 @@
 import re
+from random import randrange
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_view_page = app.contact.get_contact_info_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
